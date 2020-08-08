d38a70abf5dc275ce6f69086b1ed747ccc2db66c
Thotsana <thosana.mabotsa@umuzi.org>
2020-08-08 19:32:26 +0200

    Initial Commit

---
+++ HuliPizza/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HuliPizza
+//
+//  Created by Thotsana Mabotsa on 2020/08/08.
+//  Copyright © 2020 Thotsana Mabotsa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
