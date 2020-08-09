884df7cb026386e15d8543ac80153b9ce2d32110
Thotsana <thosana.mabotsa@umuzi.org>
2020-08-09 20:36:39 +0200

    Added a Map

---
+++ HuliPizza/HistoryView/MapView.swift
@@ -0,0 +1,36 @@
+//
+//  MapView.swift
+//  HuliPizza
+//
+//  Created by Thotsana Mabotsa on 2020/08/09.
+//  Copyright © 2020 Thotsana Mabotsa. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: UIViewRepresentable {
+	
+	var latitude: Double
+	var longitude: Double
+	var regionRadius: Double
+   
+	func makeUIView(context: Context) -> MKMapView {
+		MKMapView(frame: .zero)
+	}
+	
+	func updateUIView(_ view: MKMapView, context: Context) {
+		let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+			   view.setRegion(MKCoordinateRegion(center: coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius),animated: true)
+			   let annotation = MKPointAnnotation()
+			   annotation.coordinate = coordinate
+			   view.addAnnotation(annotation)
+	}
+	
+}
+
+//struct MapView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        MapView()
+//    }
+//}
