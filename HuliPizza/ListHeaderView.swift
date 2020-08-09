53e64a6a3ba8f7542d5ec1486ee25699c831a501
Thotsana <thosana.mabotsa@umuzi.org>
2020-08-09 04:21:25 +0200

    Binding, Published and ObservableObject

--- HuliPizza/ListHeaderView.swift
+++ HuliPizza/ListHeaderView.swift
@@ -9,21 +9,25 @@
 import SwiftUI
 
 struct ListHeaderView: View {
+	@ObservedObject var orderModel: OrderModel
 	var text: String
 	var body: some View {
 		HStack {
 			Text(text)
 				.padding(.leading, 5)
 				.foregroundColor(Color("G2"))
-				
 			Spacer()
+			Text(orderModel.formattedTotal)
+				.fontWeight(.heavy)
+				.padding(.trailing)
 		}
+		.foregroundColor(Color("IP"))
 		.background(Color("G4"))
 	}
 }
 
 struct ListHeaderView_Previews: PreviewProvider {
     static var previews: some View {
-        ListHeaderView(text: "Menu")
+        ListHeaderView(orderModel: OrderModel() , text: "Menu")
     }
 }
